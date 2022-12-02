8f631af395995cb445e02e3b63dc46cf745a8eb5
boraurora <112134966+boraurora@users.noreply.github.com>
2022-12-01 19:38:35 -0600

    Update main.cpp

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,15 +1,22 @@
-﻿//write include statements
-
-
+//write include statements
+#include<iostream>
+#include "data_types.h"
 //write namespace using statement for cout
-
+using namespace std;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
-	
-
+	int num;
+	std::cout << "Input a number:" <<endl;
+	std::cin >> num;
+	int result;
+	result = multiply_numbers(num);
+	cout << result<<endl;
+	int num1 = 4;
+	result = multiply_numbers(num1);
+	cout << result <<endl ;
 	return 0;
 }
